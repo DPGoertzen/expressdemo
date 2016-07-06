58b75da184e3c9e85a99a61443c40e20ee8e0ad7
DPGoertzen <dpgoertzen@gmail.com>
2016-07-06 15:29:40 -0500

    added a sweet song to /mats

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function(request, response){
   response.send("<h1>This is working... hopefully.</h1>")
+});
+
+app.get('/mats', function(request, response){
+  response.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, function(){
